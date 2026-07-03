20406da9afa408098650166bb3610e847dde3bea
Amado Brito <a.brito@sisalril.gob.do>
2026-07-02 21:19:19 -0400

    Fix JwtAuthGuard dependency in SuscripcionesModule

--- apps/api/src/modules/suscripciones/suscripciones.module.ts
+++ apps/api/src/modules/suscripciones/suscripciones.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { ConfiguracionModule } from '../configuracion/configuracion.module';
 import { SuscripcionesController } from './suscripciones.controller';
 import { SuscripcionesCronService } from './suscripciones-cron.service';
 import { SuscripcionesService } from './suscripciones.service';
 
 @Module({
-  imports: [ConfiguracionModule],
+  imports: [ConfiguracionModule, JwtModule.register({})],
   controllers: [SuscripcionesController],
   providers: [SuscripcionesService, SuscripcionesCronService],
   exports: [SuscripcionesService],
